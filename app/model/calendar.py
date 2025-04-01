fdf297445be097cdf323305cf05b8dd12a4beaeb
SebastianRendon <srendon521@gmail.com>
2025-04-01 18:52:04 -0500

    Cree la clase Event

--- app/model/calendar.py
+++ app/model/calendar.py
@@ -6,13 +6,38 @@ from app.services.util import generate_unique_id, date_lower_than_today_error, e
     reminder_not_found_error, slot_not_available_error
 
 
-# TODO: Implement Reminder class here
+@dataclass
+class Reminder:
+    EMAIL: ClassVar[str] = "email"
+    SYSTEM: ClassVar[str] = "system"
+    date_time: datetime
+    type: str = EMAIL
+    def __str__(self):
+        return f"Reminder on {self.date_time} of type {self.type}"
+
+@dataclass
+class Event:
+    title: str
+    description: str
+    date_: date
+    start_at: time
+    end_at: time
+    reminders: list[Reminder] = field(init = False, default_factory=list)
+    id: str = field(default_factory=generate_unique_id)
 
 
-# TODO: Implement Event class here
+    def add_reminder(self, date_time: datetime, reminder_type: str = Reminder.EMAIL):
+        new_reminder = Reminder(date_time, reminder_type)
+        self.reminders.append(new_reminder)
+
+    def delete_reminder(self, reminder_index: int):
+        if reminder_index < len(self.reminders):
+            self.reminders.pop(reminder_index)
+        else:
+            reminder_not_found_error()
+
+    def __str__(self) -> str:
+        return f"ID: {self.id}\nEvent title: {self.title}\nDescription: {self.description}\nTime: {self.start_at} - {self.end_at}"
 
 
-# TODO: Implement Day class here
 
-
-# TODO: Implement Calendar class here
